c90ed157742f97e0ae26a40ab1930cd697adbb33
rubeenaafreen20 <70516674+rubeenaafreen20@users.noreply.github.com>
2020-09-14 01:43:58 +0530

    Add files via upload

--- Assignment2/Code/Bernouli.py
+++ Assignment2/Code/Bernouli.py
@@ -4,19 +4,21 @@ sample_size=200
 
 digit=np.array([0,1,2,3,4,5,6,7,8,9])
 Frequency=np.array([22,26,22,22,20,10,14,28,16,20])
-number=int(input("Enter any number to find probability of its unit digit \n"))
+number=int(input("Enter any single digit number to find its probability  \n"))
 
-def lastDigit(n) : 
+def lastDigit(number) :
+    single=0
     # return the last digit 
-    return (n % 10)
-
-n=lastDigit(number)
-print("\n Last digit of",number,"is",n)
-
-def checkIndexDigit(n):             #check position of last digit in digit matrix
+    if (number/10)<1:
+        single=1
+    else:
+        single=2
+    return single;
+    
+def checkIndexDigit(number):             #check position of last digit in digit matrix
     c=0
     for i in range(0,len(digit)):
-        if digit[i]==n:
+        if digit[i]==number:
             c=i
     return c;
 
@@ -24,8 +26,13 @@ def checkValueFreq(k):          #accordingly check value of frequency of last di
     freq=Frequency[k]
     return freq;
 
-indexInDigitArray=checkIndexDigit(n)
-freqOfDigit=checkValueFreq(indexInDigitArray)
+n=lastDigit(number)
+if n==1:
+    indexInDigitArray=checkIndexDigit(number)
+    freqOfDigit=checkValueFreq(indexInDigitArray)
+    probability=freqOfDigit/sample_size
+    print("Probability of digit",number,"at unit place is:",probability)
 
-probability=freqOfDigit/sample_size
-print("Probability of unit digit",n,"in entered number",number,"is:",probability)
+else:
+    print("Please enter a single digit number")
+    
