ec29a7c19b2c878738bcfdadf6d0108f76f5df41
rubeenaafreen20 <70516674+rubeenaafreen20@users.noreply.github.com>
2020-09-14 20:52:28 +0530

    Add files via upload

--- Assignment2/Code/Bernouli.py
+++ Assignment2/Code/Bernouli.py
@@ -1,3 +1,8 @@
+"""
+Created on Sat Sep 12 22:32:41 2020
+Generate 200 numbers and find probability of any random digit
+@author: Rubeena
+"""
 import numpy as np
 import random
 
@@ -10,36 +15,43 @@ def Rand(start, end, num):
         res.append(random.randint(start, end)) 
   
     return res 
-num=Rand(10000000,99999999,sample_size)
-print("random numbers are",num)
-
-index=random.randint(0,199)
-number=num[index]
-print("\n A random integer among the numbers generated is", number)
+number=Rand(1,99999999,sample_size)
+print("random numbers generated are",number)
 
 digit=np.array([0,1,2,3,4,5,6,7,8,9])
 Frequency=np.array([22,26,22,22,20,10,14,28,16,20])
 
-def lastDigit(n) : 
-    # return the last digit 
-    return (n % 10)
-
-n=lastDigit(number)
-print("\n Last digit of",number,"is",n)
-
-def checkIndexDigit(n):             #check position of last digit in digit matrix
-    c=0
-    for i in range(0,len(digit)):
-        if digit[i]==n:
-            c=i
-    return c;
-
-def checkValueFreq(k):          #accordingly check value of frequency of last digit
-    freq=Frequency[k]
+def FindFreqTable(unitDigits,digit):          #accordingly check value of frequency of last digit
+    freq=np.zeros((len(digit),),dtype='int')
+    for i in range(0,len(unitDigits)):
+        for j in range(0,len(digit)):
+            if unitDigits[i]==digit[j]:
+                c=j
+                freq[c]+=1
     return freq;
 
-indexInDigitArray=checkIndexDigit(n)
-freqOfDigit=checkValueFreq(indexInDigitArray)
+def LastDigitArray(number):
+    lastDigit = []
+    for i in range(0,len(number)):
+        lastDigit.append(number[i]%10)
+    return lastDigit;
+
+unitDigits=LastDigitArray(number)
+print("Last digits of each of these numbers generated are:\n",unitDigits)
+
+freq=FindFreqTable(unitDigits,digit)
+print("frequency distribution table \n", freq)
+
+
+def checkValueFreq(index):          #accordingly check value of frequency of last digit
+    frequen=freq[index]
+    return frequen;
+
+#Chose a random last digit
+index=random.randint(0,len(digit))
+print("\n A random last digit chosen \n", index)
+
+freqOfDigit=checkValueFreq(index)
 
 probability=freqOfDigit/sample_size
-print("Probability of unit digit",n,"in entered number",number,"is:",probability)
+print("Probability of unit digit",index,"is:",probability)
