97e379034a06358f1dc4ba9840b1b9221e8a4bf9
rubeenaafreen20 <70516674+rubeenaafreen20@users.noreply.github.com>
2020-09-17 18:04:54 +0530

    Add files via upload

--- Assignment2/Code/Bernouli.py
+++ Assignment2/Code/Bernouli.py
@@ -7,13 +7,17 @@ import numpy as np
 import pandas as pd
 
 sample_size=200
+sample_size2=10000
  
 number=np.random.randint(1, 99999999, size=sample_size)
-print("random numbers generated are",number)
+number_second=np.random.randint(1,99999999, size=sample_size2)
 
 #Frequency distribution table as given in the problem in Excel sheet
 digit=np.array([0,1,2,3,4,5,6,7,8,9])
 Frequency=np.array([22,26,22,22,20,10,14,28,16,20])
+freq_given=pd.DataFrame({'Digit':digit,'Frequency':Frequency,'Probability':Frequency/sample_size})
+file_locate='Freq_Table_Given.xlsx'
+freq_given.to_excel(file_locate,'sheet1',index=False)
 
 def FindFreqTable(unitDigits,digit):          #accordingly check value of frequency of last digit
     freq1=np.zeros((len(digit),),dtype='int')
@@ -32,6 +36,7 @@ def LastDigitArray(number):
 
 #Enter Random numbers and their last digits into excel sheet
 unitDigits=LastDigitArray(number)
+unitDigits_second=LastDigitArray(number_second)
 
 #This part prints frequency of each digit on console.
 #Used only for comparison purpose
@@ -40,16 +45,22 @@ unitDigits=LastDigitArray(number)
 #print("freq of digits of random num generated is \n",d2)
 
 #Generates an excel sheet containing Frequency Distribution Table for Random numbers unit digits
-def GenerateFreqTable(digit,unitDigits):
+def GenerateFreqTable(digit,unitD,sample_space):
     freq=np.zeros((len(digit),),dtype='int')
-    for i in range(0,len(unitDigits)):
+    for i in range(0,len(unitD)):
         for j in range(0,len(digit)):
-            if unitDigits[i]==digit[j]:
+            if unitD[i]==digit[j]:
                 c=j
                 freq[c]+=1
-    freq_random=pd.DataFrame({"Digit":digit,"Frequency":freq,"Probability":freq/sample_size})
-    filepath = 'Freq_Table_generated_random.xlsx'
-    freq_random.to_excel(filepath,'sheet_random',index=False)
-    return;
+    return freq;
 
-GenerateFreqTable(digit,unitDigits)
+freq_first=GenerateFreqTable(digit,unitDigits,sample_size)
+freq_second=GenerateFreqTable(digit, unitDigits_second,sample_size2)
+
+freq_random_1=pd.DataFrame({"Digit":digit,"Frequency":freq_first,"Probability":freq_first/sample_size})
+freq_random_2=pd.DataFrame({"Digit":digit,"Frequency":freq_second,"Probability":freq_second/sample_size2})
+filepath = 'Freq_Table_for_200_numbers.xlsx'
+filepath2 = 'Freq_Table_for_10000_numbers.xlsx'
+freq_random_1.to_excel(filepath,'sheet_random',index=False)
+freq_random_2.to_excel(filepath2,'sheet_random_2',index=False)
+
